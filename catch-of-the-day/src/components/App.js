fbe979f5f4f9e2ac42a207e2acfa451cedf83fec
Simon Pioli <simon@simonpioli.com>
2020-08-07 14:49:37 +0100

    Load sample data

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
     state = {
@@ -15,14 +18,18 @@ class App extends React.Component {
         this.setState({fishes});
     }
 
+    loadSampleFishes = () => {
+        this.setState({fishes: sampleFishes});
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header tagline="Fresh Seafood Market"/>
+                    <Header tagline="Fresh Seafood Market" />
                 </div>
-                <Order/>
-                <Inventory addFish={this.addFish}/>
+                <Order />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         );
     }
