b84a14578e3f406521ca265a5405925b00c10fe1
Simon Pioli <simon@simonpioli.com>
2020-08-07 11:36:03 +0100

    Styling

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 import StorePicker from "./components/store-picker";
+import './css/style.css';
 
 render(<StorePicker/>, document.querySelector('#main'));
