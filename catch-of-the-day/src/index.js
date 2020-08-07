4058ee09b79d18528cd1de6ecc6e8e59f57d7d64
Simon Pioli <simon@simonpioli.com>
2020-08-07 12:19:32 +0100

    Scaffold components, initialise routing

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import {render} from 'react-dom';
-import StorePicker from "./components/store-picker";
-import './css/style.css';
+import React from "react";
+import {render} from "react-dom";
+import Router from "./components/Router";
+import "./css/style.css";
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
