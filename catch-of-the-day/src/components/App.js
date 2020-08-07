2f82b795d8d72320a6a4fff9b78fd40408ca63cf
Simon Pioli <simon@simonpioli.com>
2020-08-07 15:14:16 +0100

    Fish listing and adding to order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "./Header";
+import Fish from "./Fish";
 import Inventory from "./Inventory";
 import Order from "./Order";
 
@@ -22,13 +23,29 @@ class App extends React.Component {
         this.setState({fishes: sampleFishes});
     }
 
+    addToOrder = (key) => {
+        const order = {...this.state.order};
+        order[key] = order[key] + 1 || 1;
+        this.setState({order});
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => {
+                            return (<Fish
+                                key={key}
+                                index={key}
+                                details={this.state.fishes[key]}
+                                addToOrder={this.addToOrder}
+                            />);
+                        })}
+                    </ul>
                 </div>
-                <Order />
+                <Order order={this.state.order} />
                 <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         );
