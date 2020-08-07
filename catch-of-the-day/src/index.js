c452e209a6b67397934669f821654504e167fe1a
Simon Pioli <simon@simonpioli.com>
2020-08-07 11:19:28 +0100

    First component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,5 @@
-// let's go!
+import React from 'react';
+import {render} from 'react-dom';
+import StorePicker from "./components/store-picker";
+
+render(<StorePicker/>, document.querySelector('#main'));
